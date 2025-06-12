b5ecc77bc77ec8f08e3c7f7e0ba4b487474417d6
Kaique José Rodrigues <125932564+KaiqRodrigues@users.noreply.github.com>
2025-06-11 22:11:51 -0300

    Add files via upload

---
+++ PwebNode/Exercicios/app/config/dbConnection.js
@@ -0,0 +1,19 @@
+let sql = require('mssql');
+let connSQLServer = function(){
+    const sqlConfig = {
+        user:'BD2313027',
+        password: 'K1912jr@',
+        database: 'BD',
+        server: 'APOLO',
+        options:{
+            encrypt: false,
+            trustServerCertificate:true,
+    }
+}
+return sql.connect(sqlConfig);
+}
+
+module.exports =function(){
+    console.log('O autoload carregou o modulo de conexao com o BD');
+    return connSQLServer;
+}
